8153cad1b4aaa6e24e682be3f3691551e1a1f74d
Igor De Lima Nunes <igorlnunes@gmail.com>
2025-04-18 16:54:14 -0300

    cosmetic: css_rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from "./components/Heading"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export function App() {
+    return (
+        <div>
+            <Heading/>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate suscipit ullam nobis eius ducimus amet esse voluptas laborum natus veritatis quo deserunt vitae alias ab assumenda fugiat, sed, ex cumque.</p>
+        </div>
+    );
+};
